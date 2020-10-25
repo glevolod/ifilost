578b3ee63669b3dbf14b9f1c35315dcda8c3d959
vladimir <g86vladimir@gmail.com>
2020-10-25 23:34:53 +0300

    improve assets, add admin-lte

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,18 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'ionicons/package.json'
+import 'admin-lte/dist/css/adminlte.min.css'
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+require('jquery');
+require('ionicons/package.json');
+require('bootstrap');
+require('admin-lte');
+require('@fortawesome/fontawesome-free/js/all');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
